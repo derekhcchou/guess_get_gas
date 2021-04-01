67a796f3e95ccd1b05c39a0e1133400f8d5716b3
Adele Yu <fhead17@gmail.com>
2021-04-01 14:36:32 +0800

    feat: generate NFT contract

---
+++ contracts/GenerateBadge.sol
@@ -0,0 +1,69 @@
+// contracts/GenerateBadge.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract GenerateBadge is ERC721, VRFConsumerBase {
+    bytes32 public keyHash;
+    address public vrfCoordinator;
+    uint256 internal fee;
+
+    uint256 public randomResult;
+
+    struct Badge {
+        uint256 strength;
+        uint256 speed;
+        uint256 stamina;
+        string name;
+    }
+
+    Badge[] public badges;
+
+    // mappings will go here
+    mapping(bytes32 => string) requestToBadgeName;
+    mapping(bytes32 => address) requestToSender;
+    mapping(bytes32 => uint256) requestToTokenId;
+
+    constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash)
+    VRFConsumerBase(_VRFCoordinator, _LinkToken)
+    ERC721("GGGBadge", "GGGB") public {
+        vrfCoordinator = _VRFCoordinator;
+        keyHash = _keyhash;
+        fee = 0.1 * 10**18;
+    }
+
+    function requestNewRandomBadge(uint256 userProvidedSeed, string memory name) public returns (bytes32) {
+        bytes32 requestId = requestRandomness(keyHash, fee, userProvidedSeed);
+        requestToBadgeName[requestId] = name;
+        requestToSender[requestId] = msg.sender;
+        return requestId;
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
+        // define the creation of the NFT
+        uint256 newId = badges.length;
+        uint256 strength = (randomNumber % 100);
+        uint256 speed = ((randomNumber % 10000) / 100);
+        uint256 stamina = ((randomNumber % 1000000) / 100000);
+
+        badges.push(
+            Badge(
+                strength,
+                speed,
+                stamina,
+                requestToBadgeName[requestId]
+            )
+        );
+        _safeMint(requestToSender[requestId], newId);
+    }
+
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+        // require(
+        //     _isApprovedOrOwner(_msgSender(), tokenId),
+        //     "ERC721: transfer caller is not owner nor approved"
+        // );
+        _setTokenURI(tokenId, _tokenURI);
+    }
+}
