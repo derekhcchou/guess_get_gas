585a3149cbce1453ba288aaa73b9b48416305ad9
Adele Yu <fhead17@gmail.com>
2021-04-02 01:52:26 +0800

    feat: add getAnswer pseudo code

---
+++ contracts/getAnswer.sol
@@ -0,0 +1,34 @@
+// contracts/GenerateBadge.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
+
+contract getAnswer {
+// getAnswer(gameId) {
+//     return answer;
+// }
+
+// calculateAnswer(gameId) {
+//     options = getGameOptions(gameId)
+//     answer = getAnswer(gameId)
+//     options
+//     return optionId;
+// }
+
+// saveAnswer(gameId, answerId) {
+//     question = questions[gameId]
+//     question["option"] = answerId
+// }
+
+// processUserAnswers(gameId) {
+//     winners = getWinners(gameId, optionId)
+
+//     if (gameInfo[gameId]["type"] == "daily") {
+//         for (uint i = 0; i < winners.length; i++) {
+//             issueNFT(winners[i])
+//         }
+//     } else {
+//         for (uint i = 0; i < winners.length; i++) {
+//             issueCurrency(winners[i])
+//         }
+//     }
+// }
