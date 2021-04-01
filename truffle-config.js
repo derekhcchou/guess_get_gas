4c24563092387827fe7ac92cd1047b6cd74c1971
Adele Yu <fhead17@gmail.com>
2021-04-01 13:40:30 +0800

    chore: add erc721/hdwallet/dotenv modules; fix compiler version

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,29 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+require('dotenv').config()
+// 0x5F3313814F7FB3E11C4a240141689BA9933c5607
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "localhost",
       port: 7545,
       network_id: "*" // Match any network id
     },
-    develop: {
-      port: 8545
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, process.env.RINKEBY_RPC_URL)
+      },
+      network_id: "4"
     }
-  }
-};
+  },
+  compilers: {
+    solc: {
+      version: '0.6.12',
+    },
+  },
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ]
+}
