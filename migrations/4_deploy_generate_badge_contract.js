0d3d3970a1ddec86be83a359c9ca663670124e2a
Adele Yu <fhead17@gmail.com>
2021-04-11 06:00:18 +0800

    feat: add process answer

--- migrations/4_deploy_generate_badge_contract.js
+++ migrations/4_deploy_generate_badge_contract.js
@@ -2,6 +2,5 @@ const GenerateBadge = artifacts.require('GenerateBadge')
 
 module.exports = async (deployer, network, [defaultAccount]) => {
     await deployer.deploy(GenerateBadge)
-    console.log(process.env.MNEMONIC)
     let badge = await GenerateBadge.deployed()
 }
