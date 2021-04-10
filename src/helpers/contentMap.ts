54748586582b9e460505ccfdd091d697b58d1bca
Adele Yu <fhead17@gmail.com>
2021-04-11 07:46:35 +0800

    feat: copywrite of game rules and stories

--- src/helpers/contentMap.ts
+++ src/helpers/contentMap.ts
@@ -1,7 +1,38 @@
 
 export const gameRuleMap = {
-    daily: "We need to put some rules here right???  Well according to our UI layout, we should :))) So this is the rule for daily games",
-    weekly: "We need to put some rules here right???  Well according to our UI layout, we should :))) So this is the rule for weekly games",
-    monthly: "We need to put some rules here right???  Well according to our UI layout, we should :))) So this is the rule for monthly games",
-    lifetime: "We need to put some rules here right???  Well according to our UI layout, we should :))) So this is the rule for lifetime games",
+    daily: "1. How well do you know about this crypto currency? Check out the story behind it!\
+    2. Everyday, our game room will have different kinds of topics for you to join!\
+    3. You will have unlimited access to our daily games if you stake 10 USDC.\
+    4. Select an option, make your guess!\
+    5. Your choice will be locked once you submitted. Your stake will be held until the answer is revealed, you may choose to retrieve your stake whenever you're not participating the game.\
+    6. Answer will be revealed once time is up.\
+    7. Winners will get a NFT of the topic.\
+    8. Your stake will be unlocked if you didn't win and/or if you're not participating the game.",
+    weekly: "1. How well do you know about this crypto currency? Check out the story behind it!\
+    2. Everyweek, our game room will have different kinds of topics for you to join!\
+    3. Select an option, make your guess!\
+    4. Stake an amount of USDC to participate! Stake from platform balance or your connected wallet.\
+    5. Your choice will be locked once you submitted.\
+    6. Answer will be revealed once time is up.\
+    7. Winners get rewards according to formula: (Your stake / Total stake from all participants) * 0.025\
+    8. Your stake will be returned to your platform balance if you didn't win.\
+    9. Your returned stake will be returned to platform balance and can be used for next game.",
+    monthly: "1. How well do you know about this crypto currency? Check out the story behind it!\
+    2. Everyweek, our game room will have different kinds of topics for you to join!\
+    3. Select an option, make your guess!\
+    4. Stake an amount of USDC to participate! Stake from platform balance or your connected wallet.\
+    5. Your choice will be locked once you submitted.\
+    6. Answer will be revealed once time is up.\
+    7. Winners get rewards according to formula: (Your stake / Total stake from all participants) * 0.01\
+    8. Your stake will be returned to your platform balance if you didn't win.\
+    9. Your returned stake will be returned to platform balance and can be used for next game.",
+    lifetime: "1. This is an event that will happen for extremely little chance.\
+    2. This game will only refresh after event has happened\
+    3. Select an option, make your guess!\
+    4. Stake an amount of USDC to participate! Stake from platform balance or your connected wallet.\
+    5. Your choice will be locked once you submitted.\
+    6. Answer will be revealed once time is up.\
+    7. Winners get rewards according to formula: (Your stake / Total stake from all participants) * 0.012 * Life length of this game (in years).\
+    8. Your stake will be returned to your platform balance if you didn't win.\
+    9. Your returned stake will be returned to platform balance and can be used for next game."
 }
