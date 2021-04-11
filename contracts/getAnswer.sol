d80b037b64c9e580cddc67b520db2df3cecef007
DESKTOP-580I1LS\pearl <pearlwu0310@gmail.com>
2021-04-11 18:27:36 +0800

    modify contract name

--- contracts/getAnswer.sol
+++ contracts/getAnswer.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.6.0;
 
 import "https://raw.githubusercontent.com/smartcontractkit/chainlink/develop/evm-contracts/src/v0.6/ChainlinkClient.sol";
 
-contract makeQ is ChainlinkClient {
+contract getAnswer is ChainlinkClient {
 
 
 
