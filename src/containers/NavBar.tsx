ab00c221d0276f9200ab2dfdbb291d9339b4cb28
Derek Chou <derekchou@Dereks-MacBook-Air.local>
2021-04-11 14:12:13 -0400

    change navbar dropdown list color

--- src/containers/NavBar.tsx
+++ src/containers/NavBar.tsx
@@ -1,57 +1,87 @@
-import React from "react"
-import {Navbar, Button, NavDropdown} from "react-bootstrap"
-import {styles} from "../helpers/styles"
-import {Link, useLocation} from "react-router-dom";
+import React from "react";
+import { Navbar, Button, NavDropdown } from "react-bootstrap";
+import { styles } from "../helpers/styles";
+import { Link, useLocation } from "react-router-dom";
 import { IUserDataType } from "../helpers/types";
 
-
 type Props = {
-    handleShow: any;
-    hasSingedIn:boolean;
-    userData:IUserDataType;
-    handleSingOut:any;
+  handleShow: any;
+  hasSingedIn: boolean;
+  userData: IUserDataType;
+  handleSingOut: any;
 };
 
 const NavBar: React.FC<Props> = ({
-    handleShow,
-    hasSingedIn,
-    handleSingOut,
-    userData
-}) =>{
-    const dropdownTitle = `Hi, ${userData.address.slice(0,5)}...`;
-    const location = useLocation();
-    const showGameLobbyLink = !!userData.address && location.pathname !== "/gameLobby";
-    return (
-        <>
-            <Navbar style={styles.header}>
-                <Navbar.Brand href="/" style={styles.headerLogoText}>
-                    <img src="tamagochan.png" style={styles.headerLogo}></img>Guess, Get Gas
-                </Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-                    {/* Switch between "Connect Wallet" and  */}
-                    {showGameLobbyLink && 
-                    <Navbar.Text><Link to="/gameLobby" className="navbar-user-text">Back to Game Lobby</Link></Navbar.Text>
-                    }
-                    <Navbar.Text >
-                        {!hasSingedIn ? 
-                            <Button variant="outline-dark" style={styles.loginBtn} onClick={handleShow}>Connect Wallet</Button>
-                            :
-                            <>
-                                <NavDropdown title={dropdownTitle} className="navbar-user-text" id="basic-nav-dropdown">
-                                    <NavDropdown.Item> <Link to="/gameLobby" >Game Lobby</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/personalPage" > Account</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/" > About Us</Link></NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={handleSingOut} >Log off</NavDropdown.Item>
-                                </NavDropdown>
-                            </>
-                        }
-                    </Navbar.Text>
-                </Navbar.Collapse>
-            </Navbar>
-        </>
-    )
-}
+  handleShow,
+  hasSingedIn,
+  handleSingOut,
+  userData,
+}) => {
+  const dropdownTitle = `Hi, ${userData.address.slice(0, 5)}...`;
+  const location = useLocation();
+  const showGameLobbyLink =
+    !!userData.address && location.pathname !== "/gameLobby";
+  return (
+    <>
+      <Navbar style={styles.header}>
+        <Navbar.Brand href="/" style={styles.headerLogoText}>
+          <img src="tamagochan.png" style={styles.headerLogo}></img>Guess, Get
+          Gas
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          {/* Switch between "Connect Wallet" and  */}
+          {showGameLobbyLink && (
+            <Navbar.Text>
+              <Link to="/gameLobby" className="navbar-user-text">
+                Back to Game Lobby
+              </Link>
+            </Navbar.Text>
+          )}
+          <Navbar.Text>
+            {!hasSingedIn ? (
+              <Button
+                variant="outline-dark"
+                style={styles.loginBtn}
+                onClick={handleShow}
+              >
+                Connect Wallet
+              </Button>
+            ) : (
+              <>
+                <NavDropdown
+                  title={dropdownTitle}
+                  className="navbar-user-text"
+                  id="basic-nav-dropdown"
+                >
+                  <NavDropdown.Item>
+                    {" "}
+                    <Link to="/gameLobby">
+                      <label className="navbar-link">Game Lobby</label>
+                    </Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <Link to="/personalPage">
+                      <label className="navbar-link"> Account</label>
+                    </Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <Link to="/">
+                      <label className="navbar-link"> About Us</label>
+                    </Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={handleSingOut}>
+                    <label className="navbar-link">Log off</label>
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
+            )}
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+};
 
 export default NavBar;
